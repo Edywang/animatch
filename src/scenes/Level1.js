9f502d1781b2d63d34b59997e01e4ee1cc4eb37e
ericshen9999 <dragoneshen@gmail.com>
2020-05-13 21:51:21 -0700

    Adjusted Spacing

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -11,38 +11,34 @@ class Level1 extends Phaser.Scene {
     }
     create() {
         //Config
-        //let tileSelected1 = null;
-        //let tileSelected2 = null;
+        let tileSelected1 = null;
+        let tileSelected2 = null;
         //let possible = false;
         typeList = ['White','Red','Green','Blue'];
         tileArray = [
-            [1,2,3,4,5,6,7],
-            [2,2,3,4,5,6,7],
-            [3,2,3,4,5,6,7],
-            [4,2,3,4,5,6,7],
-            [5,2,3,4,5,6,7],
-            [6,2,3,4,5,6,7],
-            [7,2,3,4,5,6,7],
-            [8,2,3,4,5,6,7],
-            [9,2,3,4,5,6,7],
-            [10,2,3,4,5,6,7],
-            [11,2,3,4,5,6,7],
+            [1,2,3,4,5,6],
+            [2,2,3,4,5,6],
+            [3,2,3,4,5,6],
+            [4,2,3,4,5,6],
+            [5,2,3,4,5,6],
+            [6,2,3,4,5,6],
+            [7,2,3,4,5,6],
+            [8,2,3,4,5,6],
+            [9,2,3,4,5,6],
+            [10,2,3,4,5,6],
         ];
         //Initialize
         //Length
         for(var i = 0; i < tileArray[0].length; i++){
             //Height
             for(var j = 0; j < tileArray.length; j++){
-                var createTile = this.newTile(i,j);
-                tileArray[j][i] = createTile;
+                var type = Phaser.Math.Between(0,typeList.length-1);
+                var tempTile = new Tile(this,50*i + 90,50*j + 70,typeList[type],0).setScale(1,1).setOrigin(0, 0); 
+                tileArray[j][i] = tempTile;
             }
         }
     }
-    update() { 
-    }
-    newTile(i, j) {
-        var type = Phaser.Math.Between(0,typeList.length-1);
-        var tempTile = new Tile(this,40*i + 100,40*j + 100,typeList[type],0).setScale(1,1).setOrigin(0, 0); 
-        return tempTile;
+    update() {
+
     }
 }
